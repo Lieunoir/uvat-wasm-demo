d077db4d6a44d32c37a151f2d058d0032e7a89df
Lieunoir <33102721+Lieunoir@users.noreply.github.com>
2025-07-10 11:47:37 +0000

    Deploying to gh-pages from @ Lieunoir/uvat-wasm-demo@4c9c20c8a77f655cd50f3a46ff53df8902a67340 🚀

--- snippets/deuxfleurs-ce5ae6692bdb31f2/src/save.js
+++ snippets/deuxfleurs-ce5ae6692bdb31f2/src/save.js
@@ -1,14 +1,27 @@
 export function save_png(filename, data) {
-    const blob = new Blob([data], {type: 'image/png'});
-    if(window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveBlob(blob, filename);
-    }
-    else{
-        const elem = window.document.createElement('a');
-        elem.href = window.URL.createObjectURL(blob);
-        elem.download = filename;        
-        document.body.appendChild(elem);
-        elem.click();        
-        document.body.removeChild(elem);
-    }
+  const blob = new Blob([data], { type: "image/png" });
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveBlob(blob, filename);
+  } else {
+    const elem = window.document.createElement("a");
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = filename;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+  }
+}
+
+export function save_state(filename, data) {
+  const blob = new Blob([data], { type: "application/cbor" });
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveBlob(blob, filename);
+  } else {
+    const elem = window.document.createElement("a");
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = filename;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+  }
 }
